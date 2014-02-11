a0b6f30b0fc875aff061e3bfbccd1d6bba1ce635
bjdixon <bjdixon@gmail.com>
2014-02-11 13:59:18 -0500

    Updated settings.py adding app_name and south apps. Also added STATIC_ROOT setting

--- project_name/settings.py
+++ project_name/settings.py
@@ -36,6 +36,8 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+	'app_name',
+	'south',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -80,3 +82,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, '../static/')
+
